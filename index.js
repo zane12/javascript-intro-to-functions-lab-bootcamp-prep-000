4ddccf1978ec7ab1cda9a912b0e5f16d7300806c
Zane Armstrong <zane.armstrong@comcast.net>
2018-05-28 23:01:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,3 +13,9 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(string.toLowerCase())
 }
+
+function sayHiToGrandma(string) {
+  if (string === UpperCase) {
+    return "I can't hear you!"
+  }
+}
