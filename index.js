50ee58e359db139e5a9db9f6dfabecdbdb6b02cd
Zane Armstrong <zane.armstrong@comcast.net>
2018-05-28 23:06:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,9 +16,11 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   if (string === string.toLowerCase) {
-    return 'I can\'t hear you!'
+    return "I can't hear you!"
   }
   if (string === string.toUpperCase) {
-    
+    return "YES INDEED!"
   }
+  
+  if (string === "")
 }
